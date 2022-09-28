e105191d05400fd49ccb985141f21dffb0527696
satyaki726 <satyakisaha726@gmail.com>
2022-09-28 16:49:32 +0530

    fdgdgafgsdfsfsb

--- vars/testBuild.groovy
+++ vars/testBuild.groovy
@@ -14,7 +14,7 @@ def call(String repoUrl) {
             stage('SonarQube analysis') {
                 steps{
                     withSonarQubeEnv('sonarqube-9.5') { 
-                         bat "mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization={ORG_KEY} -Dsonar.login={GENERATED_KEY_ON_SONARCLOUD_SECURITY} -Dsonar.branch=master"
+                         bat "mvn clean package sonar:sonar"
                     }
                 }
             }
